f2791ef3cd3149b6e57a8dac38d44251e2adebe4
m01nak <m01nak77@gmail.com>
2023-02-11 21:45:18 +0530

    add jdbc implementation

--- src/main/java/com/moinak/reminder/App.java
+++ src/main/java/com/moinak/reminder/App.java
@@ -1,13 +1,35 @@
 package com.moinak.reminder;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+import com.moinak.reminder.utils.DatabaseUtils;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Connection connection = DatabaseUtils.INSTANCE.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String currentKey = null;
+        String currentValue = null;
+        try {
+            ps = connection.prepareStatement("select * from dummy");
+            rs = ps.executeQuery();
+            while(rs.next()) {
+                currentKey = rs.getString("dummy_key");
+                currentValue = rs.getString("dummy_val");
+
+                System.out.println("Key: " + currentKey + " Value: " + currentValue);
+            }
+        } catch (Exception appException) {
+            System.out.println("Error while running app! Find more below: ");
+            System.out.println(appException);
+        }
     }
 }
